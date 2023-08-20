081f7c81dd361cce30fcd934e9eba8203112f76c
Neamul01 <neamulhaque2002@gmail.com>
2023-08-21 02:21:38 +0600

    registration page reduced

--- app/auth/registration/page.tsx
+++ app/auth/registration/page.tsx
@@ -1,24 +1,8 @@
 "use client";
 
-import Layout from "@/components/Layouts/Layout/PageLayout";
 import React from "react";
-import { useRouter } from "next/navigation";
-
-const tabs = {
-  information: "Information",
-  security: "Security",
-  confirmation: "Confirmation",
-};
 export default function Page() {
-  const [active, setActive] = React.useState("information");
-  const router = useRouter;
   return (
-    <Layout className="mt-[100px] ">
-      <div className="md:w-[1000px] md:h-[767px] mx-auto ">
-        {/* {active === "information" && <InformationTab />}
-        {active === "security" && <SecurityTab />}
-        {active === "confirmation" && <InformationTab />} */}
-      </div>
-    </Layout>
+    <div className="md:w-[1000px] md:h-[767px] mx-auto ">Registration</div>
   );
 }
