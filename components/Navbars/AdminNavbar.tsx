349c1da3b728f2a4d801b848fe75d5e4dabfd861
Neamul01 <neamulhaque2002@gmail.com>
2023-08-21 11:27:33 +0600

    admin navbar modified

--- components/Navbars/AdminNavbar.tsx
+++ components/Navbars/AdminNavbar.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 
 import UserDropdown from "@/components/Admin/Dropdowns/UserDropdown";
+import AppLogo from "../common/AppLogo";
 
 export default function Navbar() {
   return (
     <>
       {/* Navbar */}
-      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 border-b shadow-sm">
-        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-          {/* Brand */}
-          <a
-            className="text-sm uppercase hidden lg:inline-block font-semibold"
-            href="#pablo"
-            // onClick={(e) => e.preventDefault()}
-          >
-            Dashboard
-          </a>
+      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center border-b shadow-sm">
+        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-[100px] px-4 md:py-[22px] py-2 bg-primary">
           {/* Form */}
-          <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
-            <div className="relative flex w-full flex-wrap items-stretch">
+          <form className="md:flex hidden flex-row flex-wrap items-center md:w-[1000px] md:h-[56px] mr-3">
+            <div className="relative flex w-full flex-wrap items-stretch rounded-lg overflow-hidden">
               <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
                 <i className="fas fa-search"></i>
               </span>
