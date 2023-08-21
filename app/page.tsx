d0ea4b3f9a0d56d2329170bd0310fae3364ee326
Neamul01 <neamulhaque2002@gmail.com>
2023-08-22 03:04:21 +0600

    student login added

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/admin/dashboard");
+    router.push("/auth/login");
   }, [router]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
