9ca3d3c4b4f2ebfe8a8c5231d40cde0810868c89
Neamul01 <neamulhaque2002@gmail.com>
2023-08-22 16:06:57 +0600

    about design changed

--- components/Admin/Account/About.tsx
+++ components/Admin/Account/About.tsx
@@ -9,12 +9,12 @@ export default function About({ user }: { user: IUser | undefined }) {
       <div className="h-[200px] w-[200px] relative flex justify-center items-center ">
         <Image src={"/"} alt="profile" fill />
       </div>
-      <h2 className="lg:text-h3 text-2xl font-semibold text-center lg:mb-12 mb-6 mt-[37px]">
+      <h2 className="xl:text-h3 text-2xl font-semibold text-center xl:mb-12 mb-6 mt-[37px]">
         {user ? user?.full_name : "Dr. Johirul Islam Nishat"}
       </h2>
       <div className="flex flex-col gap-5">
         <div className="flex flex-col gap-3">
-          <h3 className="lg:text-2xl text-xl">About</h3>
+          <h3 className="xl:text-2xl text-xl">About</h3>
           <p className="text-secondary/30 text-sm md:text-base">
             Hello I am Dr. Johirul Islam Nishat a Teacher in Job Task E-learning
             Platform. I love to study with all my Team and professors.
@@ -22,16 +22,16 @@ export default function About({ user }: { user: IUser | undefined }) {
         </div>
         <div className="flex justify-between">
           <div className="flex flex-col">
-            <h3 className="lg:text-2xl text-xl">Age</h3>
-            <p className="lg:text-[18px] text-sm text-black/50 ">17</p>
+            <h3 className="xl:text-2xl text-xl">Age</h3>
+            <p className="xl:text-[18px] text-sm text-black/50 ">17</p>
           </div>
           <div className="flex flex-col">
-            <h3 className="lg:text-2xl text-xl">Gender</h3>
-            <p className="lg:text-[18px] text-sm text-black/50 ">Male</p>
+            <h3 className="xl:text-2xl text-xl">Gender</h3>
+            <p className="xl:text-[18px] text-sm text-black/50 ">Male</p>
           </div>
         </div>
         <div className="flex flex-col">
-          <h3 className="lg:text-2xl text-xl">Date Of Birth</h3>
+          <h3 className="xl:text-2xl text-xl">Date Of Birth</h3>
           <p className="text-black/30">02/05/2006</p>
         </div>
       </div>
