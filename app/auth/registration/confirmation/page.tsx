8ca318a4d89d61ee5c425dd0197691d88af748cd
Neamul01 <neamulhaque2002@gmail.com>
2023-08-26 21:42:34 +0600

    text height changed

--- app/auth/registration/confirmation/page.tsx
+++ app/auth/registration/confirmation/page.tsx
@@ -11,11 +11,11 @@ export default function Page() {
         <Image src={"/confirm.png"} fill alt="confirm" />
       </div>
       <h3 className="md:text-h3 text-2xl capitalize mt-6 mb-4">thank you</h3>
-      <p className="max-w-[485px] text-black/40 md:text-2xl text-xl text-center mb-20">
+      <p className="max-w-[485px] text-black/40 xl:text-xl md:text-lg text-xl text-center mb-20">
         Account Has Been Created. Enjoy Job Task.
       </p>
       <button
-        className="border border-primary px-4 md:py-6 py-3 md:text-2xl md:w-[318px] rounded-lg hover:bg-primary hover:text-white "
+        className="border border-primary px-4 md:py-6 py-3 xl:text-xl md:text-lg md:w-[318px] rounded-lg hover:bg-primary hover:text-white "
         onClick={() => router.push("/admin/dashboard")}
       >
         Go to Home
